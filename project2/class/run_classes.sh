0fb24ed6bb35c6f40215e5f86af23c1c187555c9
maamari <maamari@usc.edu>
2019-11-13 18:03:11 -0600

    updated wdm file

--- project2/class/run_classes.sh
+++ project2/class/run_classes.sh
@@ -1,8 +1,9 @@
-./class inis/n0_m1e-05_s1.51991108295e-28.ini
-./class inis/n0_m0.0001_s1.51991108295e-28.ini
-./class inis/n0_m0.001_s1.51991108295e-28.ini
-./class inis/n0_m0.01_s1.51991108295e-28.ini
-./class inis/n0_m0.1_s1.51991108295e-28.ini
-./class inis/n0_m1.0_s1.51991108295e-28.ini
-./class inis/n0_m10.0_s1.51991108295e-28.ini
-./class inis/n0_m100.0_s1.51991108295e-28.ini
+./class inis/n0_m1e-06_s5.99484250319e-29.ini
+./class inis/n0_m1e-05_s5.99484250319e-29.ini
+./class inis/n0_m0.0001_s5.99484250319e-29.ini
+./class inis/n0_m0.001_s5.99484250319e-29.ini
+./class inis/n0_m0.01_s5.99484250319e-29.ini
+./class inis/n0_m0.1_s5.99484250319e-29.ini
+./class inis/n0_m1.0_s5.99484250319e-29.ini
+./class inis/n0_m10.0_s5.99484250319e-29.ini
+./class inis/n0_m100.0_s5.99484250319e-29.ini
