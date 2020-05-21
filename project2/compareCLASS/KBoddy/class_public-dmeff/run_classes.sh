485efa5b22f119ad56c44201a290c0f6b83ad30c
maamari <maamari@usc.edu>
2020-05-21 01:27:49 -0500

    add notebook

--- project2/compareCLASS/KBoddy/class_public-dmeff/run_classes.sh
+++ project2/compareCLASS/KBoddy/class_public-dmeff/run_classes.sh
@@ -1,3 +1,3 @@
-./class inis/n2_m0.0001_s1e-20.ini
-./class inis/n2_m0.0001_s1e-22.ini
-./class inis/n2_m0.0001_s1e-25.ini
+./class inis/n0_m0.0001_s1e-20.ini
+./class inis/n0_m0.0001_s1e-22.ini
+./class inis/n0_m0.0001_s1e-26.ini
